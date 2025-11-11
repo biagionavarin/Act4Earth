5643290e8b862d40a85f4852a562d3b0a99820c9
biagionavarin <biagionavarin@gmail.com>
2025-11-11 20:01:43 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -6,23 +6,17 @@ import os
 
 application = Flask(__name__)
 
-### Code GitHub
-#application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-#DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-DBVAR = 'postgresql://username:password@endpoint:5432/ebdb'
-application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
-
-### Code computer
+# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
-#DBVAR = 'sqlite:///user.db'
-#application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
-#application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
+
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
+
 
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
 login_manager= LoginManager(application)
-login_manager.login_view='users.login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 from capp.home.routes import home
