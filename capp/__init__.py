ba032c864ae5a59e714f85907c8c8b06e74f4493
biagionavarin <biagionavarin@gmail.com>
2025-11-12 18:57:49 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -9,9 +9,8 @@ application = Flask(__name__)
 # application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 
-DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}/{os.environ['RDS_DB_NAME']}"
-application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
-application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
 
 db = SQLAlchemy(application)
 bcrypt = Bcrypt(application)
@@ -27,6 +26,7 @@ application.register_blueprint(home)
 application.register_blueprint(methodology)
 application.register_blueprint(carbon_app)
 application.register_blueprint(users)
+
 
 
 
