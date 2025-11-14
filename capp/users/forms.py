83b011e2fb95e2e9b43afd0ed674fca5cdc81878
biagionavarin <biagionavarin@gmail.com>
2025-11-14 18:34:46 +0100

    Add files via upload

---
+++ capp/users/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from capp.models import User
+
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', 
+                         validators=[DataRequired(), Length(min=2, max=30)])
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  institution = SelectField('Institution',
+                              choices=[('company1', 'Company 1'),
+                                       ('company2', 'Company 2'),
+                                       ('Fantoft', 'Fantoft')],
+                              validators=[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  confirm_password = PasswordField('Confirm Password',
+                      validators=[DataRequired(), EqualTo('password')])                                
+  submit = SubmitField('Sign Up')
+
+  def validate_username(self, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user:
+        raise ValidationError('That username is taken. Please choose a different one.')
+
+  def validate_email(self, email):
+    user = User.query.filter_by(email=email.data).first()
+    if user:
+        raise ValidationError('That email is taken. Please choose a different one.')
+    
+class LoginForm(FlaskForm):
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember = BooleanField('Remember Me')
+  submit = SubmitField('Login')     
