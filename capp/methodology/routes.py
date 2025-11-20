f3d9c2bd7a052382839bf2d69000a20857f697ac
biagionavarin <biagionavarin@gmail.com>
2025-11-20 15:12:23 +0100

    Update routes.py

--- capp/methodology/routes.py
+++ capp/methodology/routes.py
@@ -5,7 +5,3 @@ methodology=Blueprint('methodology',__name__)
 @methodology.route('/methodology')
 def methodology_home():
   return render_template('methodology.html', title='methodology')
-
-@methodology.route('/paper')
-def paper():
-    return render_template('buttons/paper.html')
